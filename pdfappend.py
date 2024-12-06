e7139c6175f893b038c3966513427e739fb118b0
leojoty <gospelleseeed@gmail.com>
2024-12-06 16:45:19 -0700

    updated tagline

--- pdfappend.py
+++ pdfappend.py
@@ -1,8 +1,16 @@
+###############################################################################
+# Released under the MIT License
+# See LICENSE file in the repository root for details.
+###############################################################################
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 22 14:11:47 2024
+Created on Mon Dec 3 12:04:40 2024
 
-@author: gospe
+__author__ = "Johnathan SJ"
+__license__ = "MIT"
+__email__ = "diversity@johnangie.org"
+__status__ = "Production"
+__version__ = "1.0"
 """
 from PyPDF2 import PdfReader, PdfWriter
 
